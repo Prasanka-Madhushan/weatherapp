9ec5cb1c9a9afe3a2650df19b225f82bfe885e6c
Prasanka <prasankamadushan0@gmail.com>
2023-09-11 21:26:14 +0530

    second commit

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,25 +1,75 @@
 import React from "react";
 import './WeatherApp.css'
 
-import search from "../Assets/search.png";
-import clear from "../Assets/clear.png";
-import cloud from "../Assets/cloud.png";
-import drizzle from "../Assets/drizzle.png";
-import rain from "../Assets/rain.png";
-import snow from "../Assets/snow.png";
-import wind from "../Assets/wind.png";
-import humidity from "../Assets/humidity.png";
+import search_icon from "../Assets/search.png";
+import clear_icon from "../Assets/clear.png";
+import cloud_icon from "../Assets/cloud.png";
+import drizzle_icon from "../Assets/drizzle.png";
+import rain_icon from "../Assets/rain.png";
+import snow_icon from "../Assets/snow.png";
+import wind_icon from "../Assets/wind.png";
+import humidity_icon from "../Assets/humidity.png";
 
 const WeatherApp=()=>{
+
+    let api_key="f3632053f0b5e3842cfc92efc11aac74";
+
+    const search=async()=>{
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value==="")
+        {
+            return 0;
+        }
+        let url = 'https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&appid=${api_key}';
+
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity=document.getElementsByClassName("humidity-percent")
+        const wind=document.getElementsByClassName("wind-rate")
+        const temperature = document.getElementsByClassName("weather-temp")
+        const location = document.getElementsByClassName("weather-location");
+
+        humidity[0].innerHTML = data.main.humidity+" %";
+        wind[0].innerHTML = data.wind.speed+" km/h";
+        temperature[0].innerHTML = data.main.temp+" c";
+        location[0].innerHTML = data.name;
+    }   
+
+
     return(
         <div className="container">
             <div className="top-bar">
                 <input type="text" className="cityInput"placeholder="Search"/>
-                <div className="search">
-                    <img src={search} alt="" />
+                <div className="search-icon" onClick={()=>{search()}}>
+                    <img src={search_icon} alt="" />
                 </div>
             </div>
+            <div className="weather-image">
+                <img src={cloud_icon} alt="" />
+            </div>
+            <div className="weather-temp">
+                24 c
+            </div>
+            <div className="weather-location">
+                London
+            </div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon"/>
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
 
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon"/>
+                    <div className="data">
+                        <div className="wind-rate">18km/h</div>
+                        <div className="text">wind Speed</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
